d7744beddee8ed87774c8cd4b6d44222a1b3c854
Giorgobiani2002 <160760301+Giorgobiani2002@users.noreply.github.com>
2024-11-17 22:12:21 +0400

    4-Figma

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,73 @@
-// Figma: https://www.figma.com/design/Zxg8TR5onmWXGe19vleFdX/Final-Part3?node-id=0-1&t=hWKYRPWR9v4U8pgq-1
+"use client";
+
+import { useEffect, useState } from "react";
 
 export default function Home() {
-  return <div>Home</div>;
+  const [toggle, setToggle] = useState(false);
+  const [handle, isHandle] = useState("");
+  const [fetchApi, setFetchApi] = useState(false);
+  const [datas, setDatas] = useState();
+  console.log(toggle);
+  // console.log(handle);
+
+  useEffect(() => {
+    if (fetchApi) {
+      fetch(`http://ip-api.com/json/${handle}`)
+        .then((response) => response.json())
+        .then((data) => setDatas(data));
+    }
+  }, [fetchApi]);
+  function handleClose() {
+    setDatas(true);
+    setToggle(true);
+  }
+
+  console.log(datas);
+  return (
+    <div>
+      <div className="w-[500px] bg-black flex  flex-col items-center justify-center text-white h-[1000px] m-auto ">
+        <div className="flex gap-4 ">
+          <button
+            className="bg-[#E27125] w-[150px] h-[37px] rounded-lg"
+            onClick={() => setToggle(true)}
+          >
+            AddItem
+          </button>
+          <button
+            className="w-[83px] bg-[#19191B] h-[37px] rounded-lg"
+            onClick={handleClose}
+          >
+            Clear
+          </button>
+        </div>
+        <button
+          className="w-[83px] bg-[#19191B] h-[37px] rounded-lg mt-[20px]"
+          onClick={() => setFetchApi(true)}
+        >
+          Fetch Api
+        </button>
+        <h6>Use the button above to add items</h6>
+        {toggle && (
+          <div className="w-[259px] h-[106px]">
+            <h6>IP Address</h6>
+            <input
+              type="text"
+              className="text-black"
+              placeholder="158.143.37.141"
+              onChange={(e) => isHandle(e.target.value)}
+            />
+          </div>
+        )}
+        {datas && (
+          <div className="mt-4 text-left">
+            <h3>IP Information</h3>
+            <ul>
+              <li>{datas.country}</li>
+              <li>{datas.timezone}</li>
+            </ul>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 }
